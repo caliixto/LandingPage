0189d84b594c1d15871a5837e92d10ca8d7f5048
DESKTOP-B3V34VL\calixto <nicolas3jdd@gmail.com>
2026-06-05 14:37:22 +0200

    cambio altert notificacion

--- landingPage-front/src/app/components/formulario/formulario.component.ts
+++ landingPage-front/src/app/components/formulario/formulario.component.ts
@@ -3,6 +3,7 @@ import { Component, Output, EventEmitter } from '@angular/core';
 import { ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormGroup, FormControl } from '@angular/forms';
 import emailjs from '@emailjs/browser';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-formulario',
@@ -31,39 +32,56 @@ export class FormularioComponent {
 
     // 2. Juntamos los datos del formulario Y la nueva variable 'time'
         const datosEnvio = {
-        ...this.formulario.value, 
-          time: fechaActual  
-        };
-        emailjs.send(
-          'service_ojq8cdv',
-          'template_deg8nhy',
-          datosEnvio,
-          'B1O6K2uG_xkIXIMjt'
-        ).then((response)=> {
-          // ¡ÉXITO! El correo ya está viajando
-        console.log('¡Correo enviado con éxito!', response.status, response.text);
-        alert('¡Presupuesto solicitado correctamente! Nos pondremos en contacto contigo.');
-        
-        //Limpiamos los inputs
-        this.clear();
-        
-        //Mandamos el aviso al padre para que oculte el formulario y vuelva el botón
-        this.cerrar.emit();
+  ...this.formulario.value,
+  time: fechaActual
+};
 
-        }).catch((error) => {
-        // Por si acaso falla algo (red, límites, etc.), para que no te quedes a ciegas
-        console.error('Error al enviar el correo:', error);
-        alert('Hubo un problema al enviar el mensaje. Inténtalo de nuevo.');
-        });
-      }else {
-        // Si el usuario intenta darle al botón con campos vacíos o incorrectos
-        this.formulario.markAllAsTouched();
-      }
+emailjs.send(
+  'service_ojq8cdv',
+  'template_deg8nhy',
+  datosEnvio,
+  'B1O6K2uG_xkIXIMjt'
+).then((response) => {
+  console.log('¡Correo enviado con éxito!', response.status, response.text);
+
+  // ÉXITO: Cambiamos el alert por Swal
+  Swal.fire({
+    title: '¡Mensaje enviado!',
+    text: 'Presupuesto solicitado correctamente. Nos pondremos en contacto contigo.',
+    icon: 'success',
+    confirmButtonText: 'Aceptar',
+    confirmButtonColor: '#00d1b2'
+  });
+
+  // Limpiamos los inputs
+  this.clear();
+
+  // Mandamos el aviso al padre
+  this.cerrar.emit();
+
+}).catch((error) => {
+  console.error('Error al enviar el correo:', error);
+
+  // ERROR: Cambiamos el alert por Swal
+  Swal.fire({
+    title: 'Oops...',
+    text: 'Hubo un problema al enviar el mensaje. Inténtalo de nuevo.',
+    icon: 'error',
+    confirmButtonText: 'Cerrar',
+    confirmButtonColor: '#d33'
+  });
+});
+
+} else {
+  // Si el usuario intenta darle al botón con campos vacíos
+  this.formulario.markAllAsTouched();
+}
   }
 
   clear(){
     this.formulario.reset();
   }
+
 
   @Output() cerrar = new EventEmitter();
 
