fefe5ab5347cfa8b08863fae3ba231c31ede774d
DESKTOP-B3V34VL\calixto <nicolas3jdd@gmail.com>
2026-06-11 13:34:27 +0200

    admin

--- landingPage-front/src/app/components/projects-project/projects-project.component.ts
+++ landingPage-front/src/app/components/projects-project/projects-project.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ProjectService } from '../services-Project/project.service';
+import { ProjectService } from '../services/project.service';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
