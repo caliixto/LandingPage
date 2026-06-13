0f1be954ca8ad2d170e7b40c3d29f4e6894a929d
DESKTOP-TRHIH5M\calixto <nicolas3jdd@gmail.com>
2026-06-13 19:59:26 +0200

    listado

--- landingPage-back/index.js
+++ landingPage-back/index.js
@@ -10,7 +10,8 @@ const app = express();
 const port = process.env.PORT || 3977;
 
 // 3. Configurar CORS y, MUY IMPORTANTE, el parseo de datos ANTES de las rutas
-app.use(cors({origin: 'https://landing-page-pearl-mu-33.vercel.app/' }));
+app.use(cors({
+    origin: 'https://landing-page-pearl-mu-33.vercel.app'}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static('uploads'));
